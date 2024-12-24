f07185e3aa04ee2e0c0396d9f4107864f11a28aa
Y-Jacob Mlo <Jacobmlo.2002@gmail.com>
2024-12-24 02:20:53 -0500

    wip

--- main.tf
+++ main.tf
@@ -115,6 +115,7 @@ resource "aws_lambda_permission" "allow_api_gateway" {
 
 resource "aws_api_gateway_deployment" "weather_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.weather_api.id
+  stage_description = "prod"
   depends_on  = [
     aws_api_gateway_integration.lambda_integration,
     aws_lambda_permission.allow_api_gateway
