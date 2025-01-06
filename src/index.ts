e4214a5762578ce1461f9449194aac37a5aad42e
Y-Jacob Mlo <Jacobmlo.2002@gmail.com>
2025-01-06 11:32:39 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -110,7 +110,7 @@ export const handler = async (event: APIGatewayEvent) => {
       headers: {
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        "Access-Control-Allow-Methods": "OPTIONS,POST",
       },
       body: JSON.stringify({
         error: "API_KEY environment variable is missing",
